757772ece41fa5e7e7c085e2fd6146baec2c82fe
Marcin <94755972+Marcinzale@users.noreply.github.com>
2022-12-17 20:53:44 +0100

    UI changes - added weather icons

--- script.js
+++ script.js
@@ -14,12 +14,18 @@ function getWeather() {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=` + inputCity.value + `&units=metric&appid=` + apikey)
         .then(function (resp) { return resp.json() })
         .then(function (data) {
+            let icon = data.weather[0].icon;
+            let description = data.weather[0].description;
             let temp = data.main.temp;
             let feels_like = data.main.feels_like;
             let pressure = data.main.pressure;
 
-            document.getElementById('temp').innerHTML = temp.toLocaleString('pl').slice(0, 2) + " ºC";
-            document.getElementById('feels_like').innerHTML = feels_like.toLocaleString('pl').slice(0, 2) + " ºC";
+            const ikona = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+
+            document.getElementById('weather-icon').src = ikona;
+            document.getElementById('description').innerHTML = description.toLocaleString('pl');
+            document.getElementById('temp').innerHTML = temp.toLocaleString('pl').slice(0, 4) + " ºC";
+            document.getElementById('feels_like').innerHTML = feels_like.toLocaleString('pl').slice(0, 5) + " ºC";
             document.getElementById('pressure').innerHTML = pressure.toLocaleString('pl') + " hPa";
         })
 
